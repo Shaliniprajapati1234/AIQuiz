c88ff358e0b03332874fd45e481da7bc81e2c517
shalini-dev <shaliniprajapati2012@gmail.com>
2026-08-14 22:13:53 +0530

    Setup Express backend on port 5000

--- Server/server.js
+++ Server/server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
