1b201ec70d9304be252da3855e19375a5bd400a3
shalini-dev <shaliniprajapati2012@gmail.com>
2026-08-17 15:57:48 +0530

    Complete frontend setup and install dependencies

---
+++ client/src/lib/apiConfig.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+// export const BACKEND_BASE_URL = 'http://localhost:4000/api/v1';
+export const BACKEND_BASE_URL = 'https://backend.substrng.shop/api/v1';
+
+export const axiosClient = axios.create({
+  baseURL: BACKEND_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
